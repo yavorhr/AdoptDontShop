2d87934f0c6a8f67b4feb9c20664b56b874e53b8
wolfie <yavorhristozov@gmail.com>
2026-04-08 15:21:16 +0300

    Update InitHashedPassword.java

--- src/main/java/softuni/adoptdontshop/Init/InitHashedPassword.java
+++ src/main/java/softuni/adoptdontshop/Init/InitHashedPassword.java
@@ -1,4 +1,4 @@
-package softuni.adoptdontshop.Init;
+package softuni.adoptdontshop.init;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
